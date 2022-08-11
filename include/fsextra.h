72b7d7dfdeac43c9e218a9fb2f01973bcde6dfba
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-08-11 03:41:59 +0300

    Add  functions

--- include/fsextra.h
+++ include/fsextra.h
@@ -17,11 +17,27 @@ typedef struct {
     uint64_t depth;
 } copy_options_t;
 
-extern int fs_extra_copy_items(const char** from, size_t from_length,const char* to, const copy_options_t* options);
+typedef struct {
+    int overwrite;
+    int skip_exist;
+    size_t buffer_size;
+} copy_options_file_t;
 
-extern int fs_extra_move_items(const char** from_items, size_t from_items_length,const char* to, const copy_options_t* options);
+extern int fs_extra_copy_items(const char* const* from, size_t from_length,const char* to, const copy_options_t* options);
 
-extern int fs_extra_remove_items(const char** from_items, size_t from_items_length);
+extern int fs_extra_move_items(const char* const* from_items, size_t from_items_length,const char* to, const copy_options_t* options);
+
+extern int fs_extra_remove_items(const char* const* from_items, size_t from_items_length);
+
+extern int fs_extra_file_copy(const char* from, const char* to, const copy_options_file_t* options);
+
+extern int fs_extra_file_move_file(const char* from, const char* to, const copy_options_file_t* options);
+
+extern char* fs_extra_file_read_to_string(const char* path);
+
+extern int fs_extra_file_remove(const char* path);
+
+extern int fs_extra_file_write_all(const char* path, const char* content);
 
 #ifdef __cplusplus
 }
--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,18 @@
 use std::os::raw::c_char;
 use std::os::raw::c_int;
 use std::ffi::CStr;
+use std::ffi::CString;
 use std::slice;
 use fs_extra::copy_items;
-use fs_extra::dir::CopyOptions;
+use fs_extra::dir;
 use fs_extra::move_items;
 use fs_extra::remove_items;
+use fs_extra::file;
+use fs_extra::file::copy;
+use fs_extra::file::move_file;
+use fs_extra::file::remove;
+use fs_extra::file::read_to_string;
+use fs_extra::file::write_all;
 
 #[repr(C)]
 struct copy_options_t {
@@ -38,9 +45,16 @@ struct copy_options_t {
     depth: u64,
 }
 
+#[repr(C)]
+struct copy_options_file_t {
+    overwrite: c_int,
+    skip_exist: c_int,
+    buffer_size: usize,
+}
+
 #[no_mangle]
 unsafe extern "C" fn fs_extra_copy_items(from: *const *const c_char, from_length: usize,to: *const c_char, options: *const copy_options_t) -> c_int {
-  let opt = CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+  let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
                          skip_exist: if (*options).skip_exist == 0 { false } else { true },
                          buffer_size: (*options).buffer_size,
                          copy_inside: if (*options).copy_inside == 0 { false } else { true },
@@ -61,7 +75,7 @@ unsafe extern "C" fn fs_extra_copy_items(from: *const *const c_char, from_length
 
 #[no_mangle]
 unsafe extern "C" fn fs_extra_move_items(from_items: *const *const c_char, from_items_length: usize,to: *const c_char, options: *const copy_options_t) -> c_int {
-  let opt = CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+  let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
                          skip_exist: if (*options).skip_exist == 0 { false } else { true },
                          buffer_size: (*options).buffer_size,
                          copy_inside: if (*options).copy_inside == 0 { false } else { true },
@@ -85,6 +99,90 @@ unsafe extern "C" fn fs_extra_remove_items(from_items: *const *const c_char, fro
   let paths = slice::from_raw_parts(from_items, from_items_length);
   let vec: Vec<String> = paths.iter().map(|&i| CStr::from_ptr(i).to_string_lossy().into_owned()).collect();
   match remove_items(&vec) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_file_copy(from: *const c_char, to: *const c_char, options: *const copy_options_file_t) -> c_int {
+  let opt = file::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+                         skip_exist: if (*options).skip_exist == 0 { false } else { true },
+                         buffer_size: (*options).buffer_size };
+  let from_rs = match CStr::from_ptr(from).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+  let to_rs = match CStr::from_ptr(to).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+
+  match copy(from_rs, to_rs, &opt) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_file_move_file(from: *const c_char, to: *const c_char, options: *const copy_options_file_t) -> c_int {
+  let opt = file::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+                         skip_exist: if (*options).skip_exist == 0 { false } else { true },
+                         buffer_size: (*options).buffer_size };
+  let from_rs = match CStr::from_ptr(from).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+  let to_rs = match CStr::from_ptr(to).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+
+  match move_file(from_rs, to_rs, &opt) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_file_read_to_string(path: *const c_char) -> *mut c_char {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+      Ok(s) => s,
+      Err(_) => return std::ptr::null_mut()
+  };
+  let res = match read_to_string(path_rs) {
+    Ok(v) => v,
+    Err(_) => return std::ptr::null_mut()
+  };
+  match CString::new(res) {
+    Ok(s) => s.into_raw(),
+    Err(_) => std::ptr::null_mut()
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_file_remove(path: *const c_char) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match remove(path_rs) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_file_write_all(path: *const c_char, content: *const c_char) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  let content_rs = match CStr::from_ptr(content).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match write_all(path_rs, content_rs) {
     Ok(_) => 0,
     Err(_) => -1
   }
