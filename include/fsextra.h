8421e67a4605adb33267214fd20f41b159ffeecd
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-08-12 01:52:37 +0300

    Add list of functions

--- include/fsextra.h
+++ include/fsextra.h
@@ -15,7 +15,7 @@ typedef struct {
     int copy_inside;
     int content_only;
     uint64_t depth;
-} copy_options_t;
+} copy_options_dir_t;
 
 typedef struct {
     int overwrite;
@@ -23,9 +23,17 @@ typedef struct {
     size_t buffer_size;
 } copy_options_file_t;
 
-extern int fs_extra_copy_items(const char* const* from, size_t from_length,const char* to, const copy_options_t* options);
+typedef struct {
+  uint64_t dir_size;
+  size_t files_length;
+  size_t directories_length;
+  char** files;
+  char** directories;
+} dir_content_t;
 
-extern int fs_extra_move_items(const char* const* from_items, size_t from_items_length,const char* to, const copy_options_t* options);
+extern int fs_extra_copy_items(const char* const* from, size_t from_length,const char* to, const copy_options_dir_t* options);
+
+extern int fs_extra_move_items(const char* const* from_items, size_t from_items_length,const char* to, const copy_options_dir_t* options);
 
 extern int fs_extra_remove_items(const char* const* from_items, size_t from_items_length);
 
@@ -38,6 +46,26 @@ extern char* fs_extra_file_read_to_string(const char* path);
 extern int fs_extra_file_remove(const char* path);
 
 extern int fs_extra_file_write_all(const char* path, const char* content);
+
+extern int fs_extra_dir_copy(const char* from, const char* to, const copy_options_dir_t* options);
+
+extern int fs_extra_dir_create(const char* path, int erase);
+
+extern int fs_extra_dir_create_all(const char* path, int erase);
+
+extern int fs_extra_dir_get_dir_content(dir_content_t* dir_content, const char* path);
+
+extern int fs_extra_dir_get_dir_content2(dir_content_t* dir_content, const char* path, uint64_t* depth);
+
+extern int fs_extra_dir_get_size(const char* path, uint64_t* size);
+
+extern int fs_extra_dir_move_dir(const char* from, const char* to, const copy_options_dir_t* options);
+
+extern int fs_extra_dir_remove(const char* path);
+
+extern void fs_extra_clean_string(char* ptr);
+
+extern void fs_extra_clean_dir_content(dir_content_t* ptr);
 
 #ifdef __cplusplus
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,31 +29,45 @@ use fs_extra::dir;
 use fs_extra::move_items;
 use fs_extra::remove_items;
 use fs_extra::file;
-use fs_extra::file::copy;
 use fs_extra::file::move_file;
-use fs_extra::file::remove;
 use fs_extra::file::read_to_string;
 use fs_extra::file::write_all;
+use fs_extra::dir::create;
+use fs_extra::dir::create_all;
+use fs_extra::dir::get_dir_content;
+use fs_extra::dir::get_dir_content2;
+use fs_extra::dir::DirOptions;
+use fs_extra::dir::get_size;
+use fs_extra::dir::move_dir;
 
 #[repr(C)]
-struct copy_options_t {
-    overwrite: c_int,
-    skip_exist: c_int,
-    buffer_size: usize,
-    copy_inside: c_int,
-    content_only: c_int,
-    depth: u64,
+struct copy_options_dir_t {
+  overwrite: c_int,
+  skip_exist: c_int,
+  buffer_size: usize,
+  copy_inside: c_int,
+  content_only: c_int,
+  depth: u64,
 }
 
 #[repr(C)]
 struct copy_options_file_t {
-    overwrite: c_int,
-    skip_exist: c_int,
-    buffer_size: usize,
+  overwrite: c_int,
+  skip_exist: c_int,
+  buffer_size: usize,
+}
+
+#[repr(C)]
+struct dir_content_t {
+  dir_size: u64,
+  files_length: usize,
+  directories_length: usize,
+  files: *mut *mut c_char,
+  directories: *mut *mut c_char,
 }
 
 #[no_mangle]
-unsafe extern "C" fn fs_extra_copy_items(from: *const *const c_char, from_length: usize,to: *const c_char, options: *const copy_options_t) -> c_int {
+unsafe extern "C" fn fs_extra_copy_items(from: *const *const c_char, from_length: usize,to: *const c_char, options: *const copy_options_dir_t) -> c_int {
   let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
                          skip_exist: if (*options).skip_exist == 0 { false } else { true },
                          buffer_size: (*options).buffer_size,
@@ -74,7 +88,7 @@ unsafe extern "C" fn fs_extra_copy_items(from: *const *const c_char, from_length
 }
 
 #[no_mangle]
-unsafe extern "C" fn fs_extra_move_items(from_items: *const *const c_char, from_items_length: usize,to: *const c_char, options: *const copy_options_t) -> c_int {
+unsafe extern "C" fn fs_extra_move_items(from_items: *const *const c_char, from_items_length: usize,to: *const c_char, options: *const copy_options_dir_t) -> c_int {
   let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
                          skip_exist: if (*options).skip_exist == 0 { false } else { true },
                          buffer_size: (*options).buffer_size,
@@ -118,7 +132,7 @@ unsafe extern "C" fn fs_extra_file_copy(from: *const c_char, to: *const c_char, 
       Err(_) => return -1,
   };
 
-  match copy(from_rs, to_rs, &opt) {
+  match file::copy(from_rs, to_rs, &opt) {
     Ok(_) => 0,
     Err(_) => -1
   }
@@ -166,7 +180,7 @@ unsafe extern "C" fn fs_extra_file_remove(path: *const c_char) -> c_int {
     Ok(s) => s,
     Err(_) => return -1,
   };
-  match remove(path_rs) {
+  match file::remove(path_rs) {
     Ok(_) => 0,
     Err(_) => -1
   }
@@ -185,5 +199,198 @@ unsafe extern "C" fn fs_extra_file_write_all(path: *const c_char, content: *cons
   match write_all(path_rs, content_rs) {
     Ok(_) => 0,
     Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_copy(from: *const c_char, to: *const c_char, options: *const copy_options_dir_t) -> c_int {
+  let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+                         skip_exist: if (*options).skip_exist == 0 { false } else { true },
+                         buffer_size: (*options).buffer_size,
+                         copy_inside: if (*options).copy_inside == 0 { false } else { true },
+                         content_only: if (*options).content_only == 0 { false } else { true },
+                         depth: (*options).depth };
+  let from_rs = match CStr::from_ptr(from).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+  let to_rs = match CStr::from_ptr(to).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+
+  match dir::copy(from_rs, to_rs, &opt) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_create(path: *const c_char, erase: c_int) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match create(path_rs, if erase == 0 {false} else {true}) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_create_all(path: *const c_char, erase: c_int) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match create_all(path_rs, if erase == 0 {false} else {true}) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_get_dir_content(dir_content: *mut dir_content_t, path: *const c_char) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match get_dir_content(path_rs) {
+    Ok(mut res) => {
+      res.files.shrink_to_fit();
+      res.directories.shrink_to_fit();
+      let mut files: Vec<*mut c_char> = res.files.iter().map(|i| match CString::new(i.as_str()) {
+        Ok(s) => s.into_raw(),
+        Err(_) => std::ptr::null_mut()
+      } ).collect();
+      let mut directories: Vec<*mut c_char> = res.directories.iter().map(|i| match CString::new(i.as_str()) {
+        Ok(s) => s.into_raw(),
+        Err(_) => std::ptr::null_mut()
+      } ).collect();
+      (*dir_content).dir_size = res.dir_size;
+      (*dir_content).files_length = files.len();
+      (*dir_content).directories_length = directories.len();
+      (*dir_content).files = files.as_mut_ptr();
+      (*dir_content).directories = directories.as_mut_ptr();
+      std::mem::forget(files);
+      std::mem::forget(directories);
+      0
+    },
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_get_dir_content2(dir_content: *mut dir_content_t, path: *const c_char, depth: u64) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match get_dir_content2(path_rs, &DirOptions{
+    depth: depth
+  }) {
+    Ok(mut res) => {
+      res.files.shrink_to_fit();
+      res.directories.shrink_to_fit();
+      let mut files: Vec<*mut c_char> = res.files.iter().map(|i| match CString::new(i.as_str()) {
+        Ok(s) => s.into_raw(),
+        Err(_) => std::ptr::null_mut()
+      } ).collect();
+      let mut directories: Vec<*mut c_char> = res.directories.iter().map(|i| match CString::new(i.as_str()) {
+        Ok(s) => s.into_raw(),
+        Err(_) => std::ptr::null_mut()
+      } ).collect();
+      (*dir_content).dir_size = res.dir_size;
+      (*dir_content).files_length = files.len();
+      (*dir_content).directories_length = directories.len();
+      (*dir_content).files = files.as_mut_ptr();
+      (*dir_content).directories = directories.as_mut_ptr();
+      std::mem::forget(files);
+      std::mem::forget(directories);
+      0
+    },
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_get_size(path: *const c_char, size: *mut u64) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+    Ok(s) => s,
+    Err(_) => return -1,
+  };
+  match get_size(path_rs) {
+    Ok(v) => {
+      *size = v;
+      0
+    },
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_move_dir(from: *const c_char, to: *const c_char, options: *const copy_options_dir_t) -> c_int {
+  let opt = dir::CopyOptions{ overwrite: if (*options).overwrite == 0 { false } else { true } ,
+                         skip_exist: if (*options).skip_exist == 0 { false } else { true },
+                         buffer_size: (*options).buffer_size,
+                         copy_inside: if (*options).copy_inside == 0 { false } else { true },
+                         content_only: if (*options).content_only == 0 { false } else { true },
+                         depth: (*options).depth };
+  let from_rs = match CStr::from_ptr(from).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+  let to_rs = match CStr::from_ptr(to).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+
+  match move_dir(from_rs, to_rs, &opt) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_dir_remove(path: *const c_char) -> c_int {
+  let path_rs = match CStr::from_ptr(path).to_str() {
+      Ok(s) => s,
+      Err(_) => return -1,
+  };
+
+  match dir::remove(path_rs) {
+    Ok(_) => 0,
+    Err(_) => -1
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_clean_string(ptr: *mut c_char) {
+  if !ptr.is_null() {
+    _ = CString::from_raw(ptr);
+  }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fs_extra_clean_dir_content(ptr: *mut dir_content_t) {
+  if !ptr.is_null() {
+    if !(*ptr).files.is_null() {
+      let files = slice::from_raw_parts((*ptr).files, (*ptr).files_length);
+      for &i in files {
+        if !i.is_null() {
+          _ = CString::from_raw(i);
+        }
+      }
+      Vec::from_raw_parts((*ptr).files, (*ptr).files_length, (*ptr).files_length);
+    }
+    if !(*ptr).directories.is_null() {
+      let directories = slice::from_raw_parts((*ptr).directories, (*ptr).directories_length);
+      for &i in directories {
+        if !i.is_null() {
+          _ = CString::from_raw(i);
+        }
+      }
+      Vec::from_raw_parts((*ptr).directories, (*ptr).directories_length, (*ptr).directories_length);
+    }
   }
 }
